874c432b878fbc0a144e4a0682d24b3373b72c74
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-04-30 14:07:14 +0300

    Фронт волны, диссипация

--- main.js
+++ main.js
@@ -11,16 +11,20 @@ const CANVAS_WIDTH = 500;
 const CANVAS_HEIGHT = 500;
 
 var waveParams = {
-	frequency: 2,
-	amplitude: 20,
-	transverse: false
+	frequency: 1,
+	amplitude: 30,
+	transverse: true
 }
 
 var isRunning = false;
 
+Math.clamp = function(a,b,c) {
+	return Math.max(b,Math.min(c,a));
+}
+
 // Функция смещения для плоской волны
 function xi(x, time, amplitude, frequency) {
-	return amplitude * Math.cos(frequency * (time - x / frequency))
+	return amplitude * Math.cos(frequency * (time - x / frequency)) * Math.exp(-(time) * 0.1);
 }
 
 // Класс для узла сетки
@@ -31,33 +35,56 @@ var Node = function (x, y) {
 	this.vx = 0;
 	this.vy = 0;
 
-	this.t = 0;
+	this.t = -x / NODE_STEP;
+	this.color = {
+		r: 255,
+		g: 0,
+		b: 0,
+	}	
 
+	this.setColor = function(r, g, b) {
+		this.color = {
+			r: Math.clamp(Math.round(r), 0, 255),
+			g: Math.clamp(Math.round(g), 0, 255),
+			b: Math.clamp(Math.round(b), 0, 255),
+		}
+	}
 	this.draw = function (ctx, deltaTime) {
 		if (isRunning) {
 			this.t += deltaTime;
 		}
-
-		// Смещение
-		var offset = xi(this.x / NODE_STEP, this.t, waveParams.amplitude, waveParams.frequency);
-		if (waveParams.transverse) {
-			this.y = y + offset;
-		} else {
-			this.x = x + offset;			
-		}
-		//this.y = y + Math.cos(performance.now() * fX + x * 2) * aY;
-
-		// Цвет в зависимости от смещения
-		var distance = Math.min(1, Math.abs(offset) / waveParams.amplitude);
-		var r = Math.round(255 - 255 * distance);
-		var g = Math.round(255 * distance);
-		var b = 255;
-		ctx.strokeStyle = "rgb(" + r + ", " + g +", " + b + ")";//NODE_COLOR;
+		// if (this.x > 70) {
+		// 	return;
+		// }
+		ctx.strokeStyle = "rgb(" + this.color.r + ", " + this.color.g +", " + this.color.b + ")";
 		ctx.fillStyle = ctx.strokeStyle;
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, NODE_SIZE, 0, 2 * Math.PI);
 		ctx.fill();
 		ctx.stroke();
+		var tmul = 1;
+		if (this.t < 0) {
+			var tmul = 1 - (-this.t) / 1;
+		}
+		this.setColor(0, 150 * tmul, 255);		
+		if (this.t < 0) {
+			return;
+		}
+		// Смещение
+		var offset = -xi(Math.PI, this.t, waveParams.amplitude, waveParams.frequency)-waveParams.amplitude;
+		if (waveParams.transverse) {
+			this.y = y + offset;
+		} else {
+			this.x = x + offset;			
+		}
+
+		// Цвет в зависимости от смещения
+		//console.log(this.t)
+		// if () {
+		// 	this.setColor(255, 0, 0);
+		// } else {
+		// 	this.setColor(0, 0, 255);
+		// }//NODE_COLOR;
 	}
 }
 
@@ -65,7 +92,7 @@ var Node = function (x, y) {
 var Grid = function (width, height) {
 	this.width = width;
 	this.height = height;
-
+ 
 	this.fX = 0.002;
 	this.fY = 0.01;
 	this.aX = 20;
@@ -75,7 +102,7 @@ var Grid = function (width, height) {
 	for (var i = 0; i < width; i++) {
 		grid[i] = new Array(height);
 		for (var j = 0; j < height; j++) {
-			grid[i][j] = new Node(i * NODE_STEP + NODE_STEP / 2, j * NODE_STEP + NODE_STEP / 2);
+			grid[i][j] = new Node(i * NODE_STEP + NODE_STEP / 2, j * NODE_STEP + NODE_STEP / 2 + NODE_STEP);
 		}
 	}
 
@@ -153,5 +180,8 @@ function init() {
 	$("#wave_amplitude").change(function () {
 		var val = $(this).val();
 		waveParams.amplitude = Number(val);
-	});
+	})
+
+	$("#wave_frequency").val(waveParams.frequency);
+	$("#wave_amplitude").val(waveParams.amplitude);
 }
